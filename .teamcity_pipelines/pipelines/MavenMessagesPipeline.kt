9260f4b5b63c9cf5a430c141e78fcb8d98a0f712
Daria Krupkina <daria.krupkina@jetbrains.com>
2026-03-18 15:58:51 +0100

    Remove correct secret

--- .teamcity_pipelines/pipelines/MavenMessagesPipeline.kt
+++ .teamcity_pipelines/pipelines/MavenMessagesPipeline.kt
@@ -76,7 +76,7 @@ object MavenMessagesPipeline : Pipeline ({
             id = "AzureRegistry"
             url = "https://dkrupkinacontainerregistry.azurecr.io"
             userName = "dkrupkinacontainerregistry"
-            password = "credentialsJSON:76a85c3b-ca64-43a3-858f-b99cab07100a"
+            password = "credentialsJSON:76a85c3b-ca64-43a3-858f-bs"
         }
     }
 
